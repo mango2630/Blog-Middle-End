ee47cb174491bc7418f3a78a20d9449f9c55e18f
mango2630 <2276532630@qq.coom>
2021-08-13 19:51:23 +0800

    feat(global):the first submit

---
+++ app/middleware/adminauth.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = options => {
+  return async function adminauth(ctx, next) {
+    // console.log('adminauth/ ctx.session.openId:', ctx.session.openId);
+    if (ctx.session.openId) {
+      await next();
+    } else {
+      ctx.body = { data: 'failed' };
+    }
+  };
+};
---
+++ config/config.default.js
@@ -0,0 +1,64 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1628150922872_3483';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: 'root',
+      // database
+      database: 'react_blog',
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+    domainWhiteList: [ '*' ],
+
+  };
+
+  config.cors = {
+    // origin: 'http://locaohost:3000',
+    credentials: true, // 允许Cookie的跨域
+    allowMethods: 'GET, HEAD, PUT, POST, DELELTE, PATCH, OPTIONS',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
